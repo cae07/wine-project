f765afc6d0d9b80541ac01387a6e413f9b83c648
Cae <eduardokae01@hotmail.com>
2022-04-15 14:44:05 -0300

    feat: inicia o header com css

--- wine-project/src/components/Header.jsx
+++ wine-project/src/components/Header.jsx
@@ -1,38 +1,45 @@
 import React from 'react';
-import logo from "../images/logo.jpeg";
+import logo from '../images/logo.jpeg';
+import '../css/header.css';
 
 function Header() {
   return(
-    <div>
-      <img
-        src={ logo }
-        alt="Logo Wine - O maior clube de vinhos do mundo"
-        width="130px"
-      />
-      <button>
-        Clube
-      </button>
-      <button>
-        Loja
-      </button>
-      <button>
-        Produtores
-      </button>
-      <button>
-        Ofertas
-      </button>
-      <button>
-        Eventos
-      </button>
-      <button>
-        <i className="fas fa-search" />
-      </button>
-      <button>
-        <i className="far fa-user" />
-      </button>
-      <button>
-      <i className="fa fa-shopping-cart" aria-hidden="true" />
-      </button>
+    <div className="header-container">
+      <div>
+        <img
+          src={ logo }
+          alt="Logo Wine - O maior clube de vinhos do mundo"
+          width="100px"
+        />
+      </div>
+      <div>
+        <button id="header-buttons">
+          Clube
+        </button>
+        <button id="header-buttons">
+          Loja
+        </button>
+        <button id="header-buttons">
+          Produtores
+        </button>
+        <button id="header-buttons">
+          Ofertas
+        </button>
+        <button id="header-buttons">
+          Eventos
+        </button>
+      </div>
+      <div>
+        <button id="icons-buttons">
+          <i className="fas fa-search" />
+        </button>
+        <button id="icons-buttons">
+          <i className="far fa-user" />
+        </button>
+        <button id="icons-buttons">
+        <i className="fa fa-shopping-cart" aria-hidden="true" />
+        </button>
+      </div>
     </div>
     
   );
