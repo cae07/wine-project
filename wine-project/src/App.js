5117b396cce8609ae1a4d27500d1f8a0776436bf
Cae <eduardokae01@hotmail.com>
2022-04-15 23:00:59 -0300

    feat: tela de detalhes do vinho

--- wine-project/src/App.js
+++ wine-project/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import { Main } from './pages';
+import { Main, ProductDetails } from './pages';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={ <Main /> } />
+      <Route path="/:id" element={ <ProductDetails />} />
     </Routes>
   );
 }
--- wine-project/src/pages/index.js
+++ wine-project/src/pages/index.js
@@ -1,5 +1,7 @@
 import Main from "./Main";
+import ProductDetails from './ProductDetails';
 
 export {
   Main,
+  ProductDetails,
 }
