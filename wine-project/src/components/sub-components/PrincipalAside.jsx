d2e93082809d891797902cb4bb978eba2b7938dd
Cae <eduardokae01@hotmail.com>
2022-04-16 16:39:56 -0300

    refactor:  usa bootstrap para radio buttons

--- wine-project/src/components/sub-components/PrincipalAside.jsx
+++ wine-project/src/components/sub-components/PrincipalAside.jsx
@@ -1,32 +1,43 @@
 import React from 'react';
+import { Form } from 'react-bootstrap';
 
 function PrincipalAside() {
   return (
     <aside className="aside-container">
       <h2>Refine sua busca</h2>
       <h3>Por preço</h3>
-      <form className="radios-container">
-        <label htmlFor="fourty">
-          <input id="fourty" type="radio" name="price-search" value="fourty" />
-          Até R$40
-        </label>
-        <label htmlFor="sixty">
-          <input id="sixty" type="radio" name="price-search" value="sixty" />
-          R$40 A R$60
-        </label>
-        <label htmlFor="twoHundred">
-          <input id="twoHundred" type="radio" name="price-search" value="twoHundred" />
-          R$100 A R$200
-        </label>
-        <label htmlFor="fiveHundred">
-          <input id="fiveHundred" type="radio" name="price-search" value="fiveHundred" />
-          R$200 A R$500
-        </label>
-        <label htmlFor="fiveHundredAbove">
-          <input id="fiveHundredAbove" type="radio" name="price-search" value="fiveHundredAbove" />
-          Acima R$500
-        </label>
-      </form>
+      <Form className="radios-container">
+        <Form.Check 
+          type="radio"
+          name="price"
+          id="default-radio-1"
+          label="Até R$40"
+        />
+        <Form.Check 
+          type="radio"
+          name="price"
+          id="default-radio-2"
+          label="R$40 A R$60"
+        />
+        <Form.Check 
+          type="radio"
+          name="price"
+          id="default-radio-3"
+          label="R$100 A R$200"
+        />
+        <Form.Check 
+          type="radio"
+          name="price"
+          id="default-radio-4"
+          label="R$200 A R$500"
+        />
+        <Form.Check 
+          type="radio"
+          name="price"
+          id="default-radio-5"
+          label="Acima R$500"
+        />
+      </Form>
     </aside>
   );
 }
