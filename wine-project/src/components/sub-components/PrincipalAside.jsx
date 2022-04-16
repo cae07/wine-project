fdd86cda74b7487a2c44b9e56792a12dd21a8321
Cae <eduardokae01@hotmail.com>
2022-04-16 16:40:42 -0300

    refactor: dá um espaço entre $ e valor

--- wine-project/src/components/sub-components/PrincipalAside.jsx
+++ wine-project/src/components/sub-components/PrincipalAside.jsx
@@ -11,31 +11,31 @@ function PrincipalAside() {
           type="radio"
           name="price"
           id="default-radio-1"
-          label="Até R$40"
+          label="Até R$ 40"
         />
         <Form.Check 
           type="radio"
           name="price"
           id="default-radio-2"
-          label="R$40 A R$60"
+          label="R$ 40 A R$ 60"
         />
         <Form.Check 
           type="radio"
           name="price"
           id="default-radio-3"
-          label="R$100 A R$200"
+          label="R$ 100 A R$ 200"
         />
         <Form.Check 
           type="radio"
           name="price"
           id="default-radio-4"
-          label="R$200 A R$500"
+          label="R$ 200 A R$ 500"
         />
         <Form.Check 
           type="radio"
           name="price"
           id="default-radio-5"
-          label="Acima R$500"
+          label="Acima R$ 500"
         />
       </Form>
     </aside>
