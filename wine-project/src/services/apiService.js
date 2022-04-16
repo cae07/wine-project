61e4ab7372a55c08e538c6b7de87f8fb1e8ebc63
Cae <eduardokae01@hotmail.com>
2022-04-16 16:02:06 -0300

    refactor: serviceApi para 9 itens por pagina

--- wine-project/src/services/apiService.js
+++ wine-project/src/services/apiService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const getProducts = async (page) => {
-  const { data } = await axios.get(`https://wine-back-test.herokuapp.com/products?page=${page}&limit=10`);
+  const { data } = await axios.get(`https://wine-back-test.herokuapp.com/products?page=${page}&limit=9`);
   return data;
 };
 
