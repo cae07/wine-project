eaff74c21e66d632fdb7bc9ce710b20fa3c2bc3e
Cae <eduardokae01@hotmail.com>
2022-04-16 16:28:05 -0300

    feat: funções para filtros de api

--- wine-project/src/services/apiService.js
+++ wine-project/src/services/apiService.js
@@ -5,4 +5,18 @@ const getProducts = async (page) => {
   return data;
 };
 
-export default getProducts;
+const getProductsByPrice = async (price) => {
+  const { data } = await axios.get(`https://wine-back-test.herokuapp.com/products?filter=${price}`);
+  return data;
+};
+
+const getProductsByName = async (name) => {
+  const { data } = await axios.get(`https://wine-back-test.herokuapp.com/products?name=${name}`);
+  return data;
+};
+
+export {
+  getProducts,
+  getProductsByPrice,
+  getProductsByName,
+};
